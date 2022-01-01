43ed33ff7e0ad7511f93fa28fe356bc72cb2feea
sxg <815784691@qq.com>
2022-01-01 20:07:13 +0800

    first commit

---
+++ src/main/java/com/sxg/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.sxg.test;
+
+/**
+ * @author sxg
+ * @create 2022/1/1 20:03
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!!!");
+    }
+}
